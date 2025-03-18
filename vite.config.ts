d06364af6e3dc3fb96b75246a398a6b17f7fd32f
Jonathan Giroux <giroux.jo@gmail.com>
2025-03-18 14:29:12 +0100

    Import libraries from CDN in production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom/client'],
+      output: {
+        paths: {
+          react: 'https://esm.sh/react@19.0.0',
+          'react-dom/client': 'https://esm.sh/react-dom@19.0.0/client',
+        },
+      },
+    },
+  },
   plugins: [react()],
 });
